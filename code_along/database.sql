2efd79f28fe589baf703730ffd8356d31dc27751
Roberto C. <roberto.camacho4@gmail.com>
2025-06-05 19:14:25 +0200

    Added database file

--- code_along/database.sql
+++ code_along/database.sql
@@ -1,8 +1,11 @@
 SHOW DATABASES;
 
 CREATE DATABASE demo_db;
+CREATE DATABASE demo_db2;
 
 CREATE SCHEMA demo_db.staging;
+
+CREATE SCHEMA demo_db2.staging;
 
 CREATE TABLE customer (
     customer_id integer PRIMARY KEY,
